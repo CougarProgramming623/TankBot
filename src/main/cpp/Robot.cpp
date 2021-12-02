0b45b75d75a4ba801ddebd4802ea8e4b69af1993
Carson Graham <carson42g@gmail.com>
2021-12-02 16:32:20 -0500

    Initial commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,87 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
+
+
+namespace ohs2021 {
+
+	Robot* Robot::s_Instance = nullptr;
+
+Robot::Robot() {
+
+	s_Instance = this;
+
+}
+
+
+void Robot::RobotInit() {
+	wpi::outs() << "Robot Init Started\n";
+}
+
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want to run during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {
+	frc2::CommandScheduler::GetInstance().Run();
+}
+
+/**
+ * This function is called once each time the robot enters Disabled mode. You
+ * can use it to reset any subsystem information you want to clear when the
+ * robot is disabled.
+ */
+
+void Robot::DisabledInit() {
+	wpi::outs() << "Disabled\n";
+}
+
+void Robot::DisabledPeriodic() {
+
+}
+
+/**
+ * This autonomous runs the autonomous command selected by your {@link
+ * RobotContainer} class.
+ */
+void Robot::AutonomousInit() {
+	wpi::outs() << "Auto Init Started\n";
+}
+
+void Robot::AutonomousPeriodic() {
+
+}
+
+void Robot::TeleopInit() {
+	wpi::outs() << "Teleop Init Started\n";
+	m_DriveTrain.SetBrakeMode(true);
+}
+
+/**
+ * This function is called periodically during operator control.
+ */
+void Robot::TeleopPeriodic() {
+
+}
+
+
+}//namespace
+
+
+int main() {
+	return frc::StartRobot<ohs2020::Robot>();
+}
+
+bool CanAssertionsQuit() {
+	return true;//Maybe disable during competitions
+}
---
+++ src/main/include/Robot.h
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc2/command/Command.h>
+
+
+namespace ohs2021 {
+
+class Robot : public frc::TimedRobot {
+public:
+	Robot();
+
+	void RobotInit() override;
+	void RobotPeriodic() override;
+	void DisabledInit() override;
+	void DisabledPeriodic() override;
+	void AutonomousInit() override;
+	void AutonomousPeriodic() override;
+	void TeleopInit() override;
+	void TeleopPeriodic() override;
+	void TestPeriodic() override;
+	inline static Robot& Get() { return *s_Instance; } 
+
+
+private:
+	static Robot* s_Instance;
+};
+
+}//namespace
